00bb6ce2a1983af515632ca6ef602d1ce8ea1a67
Chris Sprague <chris@chaoscontrol.org>
2022-07-01 18:27:09 -0400

    Flyout menu

--- dev/src/js/index.js
+++ dev/src/js/index.js
@@ -1,1 +1,30 @@
 import 'bootstrap'
+console.log('asdasdasdsad')
+document.addEventListener("DOMContentLoaded", function () {
+  // make it as accordion for smaller screens
+  // if (window.innerWidth < 768) {
+console.log('DOMContentLoaded')
+    document.querySelectorAll('.sidebar-left  .nav-link').forEach(function (element) {
+
+      element.addEventListener('click', function (e) {
+console.log('click')
+        // let nextEl = element.nextElementSibling;
+        // let parentEl = element.parentElement;
+        // let allSubmenus_array = parentEl.querySelectorAll('.submenu');
+
+        // if (nextEl && nextEl.classList.contains('submenu')) {
+        //   e.preventDefault();
+        //   if (nextEl.style.display == 'block') {
+        //     console.log('displa none')
+        //     nextEl.style.display = 'none';
+        //   } else {
+        //     console.log('displa block')
+        //     nextEl.style.display = 'block';
+        //   }
+        // }
+      });
+    })
+  // }
+  // end if innerWidth
+});
+// DOMContentLoaded  end
